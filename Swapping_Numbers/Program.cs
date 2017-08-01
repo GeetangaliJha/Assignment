1196ad98190eb1d4139f328b48d2fee520fcb92c
gpawan <gpawan@cisco.com>
2017-08-01 21:38:27 +0530

    msg

---
+++ Swapping_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_Numbers
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            //Create a new class for swappping nunmbers.
+            //Call swapping function from here.
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("After swap a = {0} and b = {1}", a, b);
+            Console.ReadLine();
+            a = a + b; // a * b
+            b = a - b; // a / b
+            a = a - b; // a / b
+
+            Console.WriteLine("After swap a = {0} and b = {1}", a, b);
+            Console.ReadLine();
+
+        }
+    }
+}
